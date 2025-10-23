bd268f9b7f622c6d6310ac9938902ed4a1b8d9ee
Sabrina B. <sabrina.b@nebulae-design.com>
2025-10-23 11:41:03 +0200

    feat(Admin Panel & Charts)

---
+++ lang/en/users.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'fields' => [
+        'user' => 'Utilisateur',
+        'users' => 'Utilisateurs',
+        'name' => 'Nom',
+        'email' => 'Email',
+        'email_verified_at' => 'Email vérifié le',
+        'password' => '',
+        'password_confirmation' => '',
+        'role' => 'Rôle'
+    ]
+];
